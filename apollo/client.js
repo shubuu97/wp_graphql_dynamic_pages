022c1af86f63886d95bb9352a52101d2cbe8bef5
Shubham Chitransh <shubhamchitransh8@gmail.com>
2020-09-08 21:42:09 +0530

    Fix caching issue in menus

--- apollo/client.js
+++ apollo/client.js
@@ -3,14 +3,28 @@ import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 /**
  * Instantiate required constructor fields
  */
-const cache = new InMemoryCache();
+
+const TypePolicy = {
+  //we need to pass our unique identifier, and in our case it is label
+  keyFields: ["edges", [["node", ["label"]]]],
+};
+
+const options = {
+  //to customize the cache's behavior on a type-by-type basis
+  typePolicies: {
+    // the key should be __typename
+    RootQueryToMenuItemConnection: TypePolicy,
+  },
+};
+
+const cache = new InMemoryCache(options);
 const link = createHttpLink({
-   uri: process.env.NEXT_PUBLIC_MENU_ITEMS_URL,
+  uri: process.env.NEXT_PUBLIC_MENU_ITEMS_URL,
 });
 
 const client = new ApolloClient({
-   cache,
-   link,
+  cache,
+  link,
 });
 
 export default client;
